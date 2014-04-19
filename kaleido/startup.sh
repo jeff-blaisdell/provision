ff5daf84fe7d58758d0ae60ce47dc2a16d91708d
Jeff <jeff.blaisdell@gmail.com>
2014-04-19 13:04:54 -0500

    Moved directory

--- kaleido/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo 'Running chef...'
-pushd /opt/chef-solo/provisioning/chef
-sudo ./chef-setup.sh
-sudo ./chef-execute.sh /opt/chef-solo/provisioning/chef/solo-nodes/local/kaleido-stack.json kaleido
-echo 'Finished chef...'
-popd
