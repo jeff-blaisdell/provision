f2464091ddbb094568d3349d5328e9e31674fef9
Jeff <jeff.blaisdell@gmail.com>
2014-05-01 19:07:36 -0500

    Bootstrap Artifactory security.

--- vagrant/provisioning/chef/cookbooks/kaleido/recipes/artifactory.rb
+++ vagrant/provisioning/chef/cookbooks/kaleido/recipes/artifactory.rb
@@ -5,6 +5,24 @@ artifactory_service '3.2.0' do
 	action :create
 end
 
+# Stop service
+service "artifactory" do
+  action :stop
+end
+
+# Bootstrap security configuration.
+cookbook_file "/etc/opt/jfrog/artifactory/security.import.xml" do
+  source "artifactory-security.import.xml"
+  mode 0660
+  owner "artifactory"
+  group "artifactory"
+end
+
+# Start service.
+service "artifactory" do
+  action :start
+end
+
 # Setup reverse proxy for domain http://artifactory.kaleido.io
 template "/etc/nginx/sites-available/artifactory" do
   source "nginx-artifactory.erb"
